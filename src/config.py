ba247d7bb4063002123507c271d8ea7778555df7
teep93 <rorryke93@hotmail.com>
2024-06-24 06:06:19 +0800

    initial setup of repo

---
+++ src/config.py
@@ -0,0 +1,17 @@
+from os import environ
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_marshmallow import Marshmallow
+
+class Base(DeclarativeBase):
+    pass
+
+app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
